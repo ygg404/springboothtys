17f38f9f35f8c919fd9586812c6c44bc62e5fab7
ygg404 <2279834998@qq.com>
2019-07-07 22:18:37 +0800

    springboot 后台管理

---
+++ src/main/java/io/renren/modules/sys/dao/SysUserDao.java
@@ -0,0 +1,38 @@
+
+
+package io.renren.modules.sys.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+import io.renren.modules.sys.entity.SysUserEntity;
+
+/**
+ * 系统用户
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Mapper
+public interface SysUserDao extends BaseMapper<SysUserEntity> {
+
+	/**
+	 * 查询用户的所有权限
+	 * 
+	 * @param userId 用户ID
+	 */
+	List<String> queryAllPerms(Long userId);
+
+	/**
+	 * 查询用户的所有菜单ID
+	 */
+	List<Long> queryAllMenuId(Long userId);
+
+	/**
+	 * 根据用户名，查询系统用户
+	 */
+	SysUserEntity queryByUserName(String username);
+
+}
